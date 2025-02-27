371cc023da1a30b4be6a821f17ffb81b00dc86ae
elsuraj <elsuraj21@gmail.com>
2025-02-27 12:38:01 +0100

    update

--- src/veriflux_frontend/src/pages/IssuancePage.jsx
+++ src/veriflux_frontend/src/pages/IssuancePage.jsx
@@ -2,12 +2,9 @@
 import React, { useCallback, useState } from "react";
 
 import { verifluxActor } from "../../../agent.js";
-import {FileUploader} from "../";
-import ProfilePage from "./"
+import FileUploader from "../components/FileUploader.jsx";
 import { toast } from "react-toastify";
 // import {certificateService} from "../../services/apiService.tsx";
-
-
 
 function IssuancePage() {
   const [formData, setFormData] = useState({
@@ -16,61 +13,63 @@ function IssuancePage() {
     program: "",
     issuedAt: "",
   });
-  
 
   const [file, setFile] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
- 
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
-  
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setIsLoading(true);
     //input validatiion
-    if(!formData.issuer || !formData.recipient || !formData.program || !formData.issuedAt || !file){
+    if (
+      !formData.issuer ||
+      !formData.recipient ||
+      !formData.program ||
+      !formData.issuedAt ||
+      !file
+    ) {
       toast.error("Please fill all fields");
       setIsLoading(false);
       return;
     }
 
-  
     try {
       const fileReader = new FileReader();
       fileReader.onload = async () => {
         const fileData = fileReader.result;
         //call the issueCertificate function from the agent.js
-      const response = await verifluxActor.issueCertificate(
-        formData.issuer,
-        formData.recipient,
-        formData.program,
-        parseInt(new Date(formData.issuedAt).getTime() / 1000), // Convert date to seconds
-        fileData //pass the file data
-      );
-      toast.success("Certificate issued successfully: " + response);
-      setFormData({
-        issuer: "",
-        recipient: "",
-        program: "",
-        issuedAt: "",
-      });
-      setFile(null); //reset the file
-    };
-    fileReader.readAsDataURL(file); //read the file as data URL
+        const response = await verifluxActor.issueCertificate(
+          formData.issuer,
+          formData.recipient,
+          formData.program,
+          parseInt(new Date(formData.issuedAt).getTime() / 1000), // Convert date to seconds
+          fileData //pass the file data
+        );
+        toast.success("Certificate issued successfully: " + response);
+        setFormData({
+          issuer: "",
+          recipient: "",
+          program: "",
+          issuedAt: "",
+        });
+        setFile(null); //reset the file
+      };
+      fileReader.readAsDataURL(file); //read the file as data URL
     } catch (error) {
       toast.error("Issuance failed: " + error.message);
       console.error("error issuing certificate", error);
-    }finally{
+    } finally {
       setIsLoading(false);
     }
   };
 
-  
   return (
-    <main className="flex h-screen justify-center items-center">
-      <div className="container mx-auto p-4 justify-center">
+    <main className="flex h-screen justify-center items-center bg-[url(./public)]">
+      <div className="container w-auto mx-auto p-4 items-center bg-blue-200 rounded-lg shadow-lg">
         <h2 className="text-2xl font-bold mb-4">Issue a New Certificate</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
           <label className="block">
@@ -128,13 +127,13 @@ function IssuancePage() {
               className="w-auto p-2 border rounded"
             /> */}
             <FileUploader
-                  fieldChange={field.onChange}
-                  mediaUrl={post?.imageUrl}
-                />
+              // fieldChange={field.onChange}
+              mediaUrl={file?.imageUrl}
+            />
           </label>
           <button
             type="submit"
-            className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+            className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600 "
           >
             Issue Certificate
           </button>
